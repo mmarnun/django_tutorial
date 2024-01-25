9f9aa008516b78070e5df3d72e699e55ffd4e05a
mmarnun <alejandromanuelmartin03@gmail.com>
2024-01-25 17:34:37 +0000

    Desarrollo funcionando

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
+STATIC_ROOT = '/var/www/html/django_tutorial/static/'
